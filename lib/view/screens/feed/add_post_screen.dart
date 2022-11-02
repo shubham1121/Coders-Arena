8887c964a66697be23bdfcf0b90c5a7a657d1a8a
Subhojeet Sahoo <alphaisgod1@gmail.com>
2022-11-02 15:57:59 +0530

    added opacity to the second image box and display number of images picked.

--- lib/view/screens/feed/add_post_screen.dart
+++ lib/view/screens/feed/add_post_screen.dart
@@ -98,112 +98,202 @@ class _AddPostScreenState extends State<AddPostScreen> {
               ),
             ),
             spaceProvider.getHeightSpace(context, 0.04),
-            _imagesList.length > 2
-                ? SizedBox(
-                    height: displayWidth(context) * 0.4,
-                    child: GridView.builder(
-                      shrinkWrap: true,
-                      scrollDirection: Axis.vertical,
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                        crossAxisSpacing: 10,
-                      ),
-                      itemCount: 3,
-                      itemBuilder: (context, index) {
-                        return index == 0
-                            ? Container(
-                                height: 100,
-                                width: 100,
-                                child: Image.file(
-                                  _imagesList[index],
-                                  fit: BoxFit.cover,
-                                ),
-                              )
-                            : index == 1
-                                ? Container(
+            // Row can contain at most three boxes according to conditions
+            // Case 1 : If no image is picked ( _imageList.length == 0 ) , then only a box to add images will appear
+            // Case 2 : If only one image is picked , two boxes -> one will be the image and second will be the box to add more
+            // Case 3 : If images picked are 2 or more than 2
+            // Case 3.1 -> If there are only 2 images in the list then display three boxes
+            //          -> First and second will be image boxes and third will be the box to add more
+            // Case 3.2 -> If there are more than 2 images , display three boxes
+            //          -> First will be image box , second will be a stack with two children and third will be box to add more
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                // First image box
+                _imagesList.isNotEmpty
+                    ? Image.file(
+                        _imagesList.first,
+                        height: 100,
+                        width: 100,
+                        fit: BoxFit.cover,
+                      )
+                    : const SizedBox(),
+                // Horizontal space only if first image box is present
+                _imagesList.isNotEmpty
+                    ? const SizedBox(
+                        width: 10,
+                      )
+                    : const SizedBox(),
+                //
+                _imagesList.length >= 2
+                    ? _imagesList.length == 2
+                        ? Image.file(
+                            _imagesList[1],
+                            height: 100,
+                            width: 100,
+                            fit: BoxFit.cover,
+                          )
+                        : Container(
+                            height: 100,
+                            width: 100,
+                            child: Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                Opacity(
+                                  opacity: 0.2,
+                                  child: Image.file(
+                                    _imagesList[1],
                                     height: 100,
                                     width: 100,
-                                    child: Stack(
-                                      alignment: AlignmentDirectional.center,
-                                      children: [
-                                        Image.file(
-                                          _imagesList[index],
-                                          fit: BoxFit.cover,
-                                        ),
-                                        Positioned.fill(
-                                          child: BackdropFilter(
-                                            filter: ImageFilter.blur(
-                                              sigmaX: 5,
-                                              sigmaY: 5,
-                                            ),
-                                            child: Container(
-                                              color:
-                                                  Colors.black.withOpacity(0),
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  )
-                                : Container(
-                                    height: 100,
-                                    width: 100,
-                                    color: Colors.red,
-                                    child: InkWell(
-                                      onTap: () {
-                                        chooseImage();
-                                      },
-                                      child: Icon(
-                                        Icons.add,
-                                        color: Colors.white,
-                                        size: displayWidth(context) * 0.2,
-                                      ),
-                                    ),
-                                  );
-                      },
-                    ),
-                  )
-                : SizedBox(
-                    height: displayWidth(context) * 0.4,
-                    child: GridView.builder(
-                      shrinkWrap: true,
-                      scrollDirection: Axis.vertical,
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                        crossAxisSpacing: 10,
-                      ),
-                      itemCount: _imagesList.length + 1,
-                      itemBuilder: (context, index) {
-                        return index == _imagesList.length
-                            ? Container(
-                                height: 100,
-                                width: 100,
-                                color: Colors.red,
-                                child: InkWell(
-                                  onTap: () {
-                                    chooseImage();
-                                  },
-                                  child: Icon(
-                                    Icons.add,
-                                    color: Colors.white,
-                                    size: displayWidth(context) * 0.2,
+                                    fit: BoxFit.cover,
                                   ),
                                 ),
-                              )
-                            : Container(
-                                height: 100,
-                                width: 100,
-                                // color: Colors.greenAccent,
-                                child: Image.file(
-                                  _imagesList[index],
-                                  fit: BoxFit.cover,
+                                Text(
+                                  '+${_imagesList.length}',
+                                  style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w400),
+                                  textAlign: TextAlign.center,
                                 ),
-                              );
-                      },
+                              ],
+                            ),
+                          )
+                    : const SizedBox(),
+                // Horizontal space only if second image box is present
+                _imagesList.isNotEmpty
+                    ? const SizedBox(
+                        width: 10,
+                      )
+                    : const SizedBox(),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.red,
+                  child: InkWell(
+                    onTap: () {
+                      chooseImage();
+                    },
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: displayWidth(context) * 0.2,
                     ),
                   ),
+                ),
+              ],
+            ),
+
+            const SizedBox(
+              height: 40,
+            ),
+            // _imagesList.length > 2
+            //     ? SizedBox(
+            //         height: displayWidth(context) * 0.4,
+            //         child: GridView.builder(
+            //           shrinkWrap: true,
+            //           scrollDirection: Axis.vertical,
+            //           gridDelegate:
+            //               const SliverGridDelegateWithFixedCrossAxisCount(
+            //             crossAxisCount: 3,
+            //             crossAxisSpacing: 10,
+            //           ),
+            //           itemCount: 3,
+            //           itemBuilder: (context, index) {
+            //             return index == 0
+            //                 ? Container(
+            //                     height: 100,
+            //                     width: 100,
+            //                     child: Image.file(
+            //                       _imagesList[index],
+            //                       fit: BoxFit.cover,
+            //                     ),
+            //                   )
+            //                 : index == 1
+            //                     ? Container(
+            //                         height: 100,
+            //                         width: 100,
+            //                         child: Stack(
+            //                           alignment: AlignmentDirectional.center,
+            //                           children: [
+            //                             Image.file(
+            //                               _imagesList[index],
+            //                               fit: BoxFit.cover,
+            //                             ),
+            //                             Positioned.fill(
+            //                               child: BackdropFilter(
+            //                                 filter: ImageFilter.blur(
+            //                                   sigmaX: 5,
+            //                                   sigmaY: 5,
+            //                                 ),
+            //                                 child: Container(
+            //                                   color:
+            //                                       Colors.black.withOpacity(0),
+            //                                 ),
+            //                               ),
+            //                             ),
+            //                           ],
+            //                         ),
+            //                       )
+            //                     : Container(
+            //                         height: 100,
+            //                         width: 100,
+            //                         color: Colors.red,
+            //                         child: InkWell(
+            //                           onTap: () {
+            //                             chooseImage();
+            //                           },
+            //                           child: Icon(
+            //                             Icons.add,
+            //                             color: Colors.white,
+            //                             size: displayWidth(context) * 0.2,
+            //                           ),
+            //                         ),
+            //                       );
+            //           },
+            //         ),
+            //       )
+            //     : SizedBox(
+            //         height: displayWidth(context) * 0.4,
+            //         child: GridView.builder(
+            //           shrinkWrap: true,
+            //           scrollDirection: Axis.vertical,
+            //           gridDelegate:
+            //               const SliverGridDelegateWithFixedCrossAxisCount(
+            //             crossAxisCount: 3,
+            //             crossAxisSpacing: 10,
+            //           ),
+            //           itemCount: _imagesList.length + 1,
+            //           itemBuilder: (context, index) {
+            //             return index == _imagesList.length
+            //                 ? Container(
+            //                     height: 100,
+            //                     width: 100,
+            //                     color: Colors.red,
+            //                     child: InkWell(
+            //                       onTap: () {
+            //                         chooseImage();
+            //                       },
+            //                       child: Icon(
+            //                         Icons.add,
+            //                         color: Colors.white,
+            //                         size: displayWidth(context) * 0.2,
+            //                       ),
+            //                     ),
+            //                   )
+            //                 : Container(
+            //                     height: 100,
+            //                     width: 100,
+            //                     // color: Colors.greenAccent,
+            //                     child: Image.file(
+            //                       _imagesList[index],
+            //                       fit: BoxFit.cover,
+            //                     ),
+            //                   );
+            //           },
+            //         ),
+            //       ),
             ElevatedButton(
               onPressed: () async {},
               style: ElevatedButton.styleFrom(
